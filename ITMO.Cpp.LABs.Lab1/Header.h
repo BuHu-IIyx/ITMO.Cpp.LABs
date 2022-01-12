9ce99aae00f18e73bb499b4a874b1bd6a36469f9
Alexander Vas <buhu_iiux@mail.ru>
2022-01-12 19:27:12 +0300

    1

---
+++ ITMO.Cpp.LABs.Lab1/Header.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Lab1
+{
+	void ex1();
+	void ex2();
+	void ex3();
+	void controlEx();
+}
---
+++ ITMO.Cpp.LABs.Lab1/Main.cpp
@@ -0,0 +1,12 @@
+#include "Header.h"
+#include <iomanip>
+
+int main()
+{	
+	system("chcp 1251");
+	//Lab1::ex1();
+	//Lab1::ex2();
+	//Lab1::ex3();
+	Lab1::controlEx();
+	return 0;
+}
